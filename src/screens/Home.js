d40c507b889673b2a38d7a01e3c4f1f950e31a72
TejasVKolhe <kolhetv22.comp@coeptech.ac.in>
2024-03-30 08:40:21 +0530

    Getting started

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <Navbar/>
+        <div>Hello BhaiLog</div>
+        <Footer/>
+    </div>
+  )
+}
